ef369dab045f1361013c8175134c1b7201f12b14
Bappi <bappibagh5@gmail.com>
2026-01-03 01:26:18 +0600

    Add Woo API test page

---
+++ app/test/page.tsx
@@ -0,0 +1,15 @@
+import { getCategories, getProducts } from "../../../lib/woo";
+
+const format = (value: unknown) => JSON.stringify(value, null, 2);
+
+export default async function TestPage() {
+  const categories = await getCategories();
+  const products = await getProducts();
+
+  return (
+    <main>
+      <pre>{format(categories)}</pre>
+      <pre>{format(products)}</pre>
+    </main>
+  );
+}
