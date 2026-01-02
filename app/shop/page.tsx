143a0da439c51329034c23ff86d8be96ecf7ff31
Bappi <bappibagh5@gmail.com>
2026-01-03 01:32:36 +0600

    Add Woo API test page

---
+++ app/shop/page.tsx
@@ -0,0 +1,21 @@
+import { getCategories } from "../../../lib/woo";
+
+export default async function ShopPage() {
+  const categories = await getCategories();
+
+  return (
+    <main>
+      <h1>Categories</h1>
+      <ul>
+        {Array.isArray(categories) &&
+          categories.map((category: any) => (
+            <li key={category.id ?? category.slug}>
+              <div>Name: {category.name}</div>
+              <div>Slug: {category.slug}</div>
+              <div>Count: {category.count}</div>
+            </li>
+          ))}
+      </ul>
+    </main>
+  );
+}
